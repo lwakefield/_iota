308322b07b533e890573c0530030789b7c211125
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-08-20 18:41:15 -0400

    - work on passing eslint

--- src/scope.js
+++ src/scope.js
@@ -17,7 +17,7 @@
  *   fn();
  *   // Fred
  */
-export default function exposeScope (code, scope=null, ...toExpose) {
+export default function exposeScope (code, scope = null, ...toExpose) {
     if (code instanceof Function) {
         code = `return (${code.toString()}).bind(this)()`;
     }
@@ -25,16 +25,17 @@ export default function exposeScope (code, scope=null, ...toExpose) {
         .filter(v => v.length)
         .join(',');
 
-    let aliases = toExpose.map((v, k) => `__alias${k}`)
+    let aliases = toExpose.map((v, k) => `__alias${k}`);
     let aliasRefs = [].concat(toExpose.map((v, k) => {
-        return Object.keys(v).map(v1 => `__alias${k}.${v1}`)
+        return Object.keys(v).map(v1 => `__alias${k}.${v1}`);
     }))
         .filter(v => v.length)
         .join(',');
 
+    // eslint-disable-next-line
     return new Function(...aliases, `
         return (function (${params}) {
             ${code}
         }).bind(this)(${aliasRefs});
-    `).bind(scope, ...toExpose)
+    `).bind(scope, ...toExpose);
 }
