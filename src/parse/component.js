9535fe16506c965f67772a2334dde6915b041203
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-09-18 09:48:34 -0400

    Adds some documentation

--- src/parse/component.js
+++ src/parse/component.js
@@ -2,6 +2,23 @@ import { templates } from 'components';
 
 const toArray = v => [].slice.call(v);
 
+/**
+ * Given a DOM node, we collect all attributes that start with a colon assuming
+ * that they are props.
+ *
+ * <my-component :my-prop="propToPass"></my-component>
+ *
+ * collecting props on the above element will return a function:
+ *
+ * function anonymous() {
+ *     return {
+ *         'my-prop': propToPass
+ *     }
+ * }
+ *
+ * When calling this anonymous function with the correct scope, gives us a way
+ * to evaluate the props and pass them into the component.
+ */
 export function collectProps (el) {
     const tagName = el.tagName.toLowerCase();
     const template = templates[tagName];
